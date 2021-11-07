508604cc5d197c7d7592d9283f973365f4048a7d
Aditya <aditya.tawade@somaiya.edu>
2021-11-08 00:12:52 +0530

    added deployment setup

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express= require("express")
 var cookieParser = require('cookie-parser')
 var cors = require('cors')
-
+const path = require("path")
 const app =express()
 require("./config/mongoose")
 
@@ -12,7 +12,13 @@ app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use(cors())
 
+app.use(express.static(path.join(__dirname, "frontend", "build")))
+
 app.use("/api/user",require("./routes/user"))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 
 app.listen(process.env.port,()=>{
