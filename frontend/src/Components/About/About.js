abaca3ed5f31a64033046af98ff23037b7d0175d
Vedant Sarnobat <vedant.sarnobat@somaiya.edu>
2021-12-21 23:46:27 +0530

    about

--- frontend/src/Components/About/About.js
+++ frontend/src/Components/About/About.js
@@ -1,21 +1,55 @@
-import axios from 'axios'
+import HPlatform, { HMap, HMapPolygon } from "react-here-map";
+import mapImg from '../../Media/region.png'
 
 function About() {
 
-    const APP_CODE_HERE = process.env.REACT_APP_HERE_API;
+    const polygonPoints = [52, 13, 100, 48, 2, 100, 48, 16, 100, 52, 13, 100];
+//     [
+// 19.59474673734081,73.54693936040252,100,
+// 19.58957160917866,72.89599944133037,100,
+// 18.982943159694592,73.53320645071744,100,
+// // 19.003719546254896,72.819095147094100,
+// 19.59474673734081,73.54693936040252,100
+//     ];
+    
+    const polygonOptions = {
+    style: {
+        fillColor: "transparent",
+        strokeColor: "#829",
+        lineWidth: 3,
+    },
+    };
 
-    const getMapImage = () => {
-        axios.get('https://image.maps.ls.hereapi.com/mia/1.6/mapview', {
-            'params': {
-                'bbox':'19.59474673734081,73.54693936040252,19.003719546254896,72.819095147094',
-                'apiKey': APP_CODE_HERE
-            }
-        }).then((res)=> {
-            console.log('data:', res);
-        })
-    }
     return (
-        <h1>About</h1>
+        <>
+            <h1>About</h1>
+            <HPlatform
+                app_id={process.env.REACT_APP_HERE_APPID}
+                // app_code="YOUR_APP_CODE"
+                apikey={process.env.REACT_APP_HERE_API}
+                useCIT
+                useHTTPS
+                includeUI
+                includePlaces
+            >
+            <HMap
+                style={{
+                height: "400px",
+                width: "800px",
+                }}
+                mapOptions={{ center: { lat: 52.5321472, lng: 13.3935785 } 
+                    // { lat: 19.302092225282657, lng: 73.1926302905278 }
+                 }}
+            >
+                <HMapPolygon
+                points={polygonPoints}
+                options={polygonOptions}
+                setViewBounds="true"
+                />
+            </HMap>
+            </HPlatform>;
+            <img src={mapImg}/>
+        </>
     );
 }
 
