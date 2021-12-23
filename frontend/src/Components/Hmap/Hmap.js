390f8962c4c374e79dda9977573bee62820a8cef
Omkar Sawant <omkar18@somaiya.edu>
2021-12-24 00:40:50 +0530

    estimation apis connected

--- frontend/src/Components/Hmap/Hmap.js
+++ frontend/src/Components/Hmap/Hmap.js
@@ -3,11 +3,28 @@ import { DisplayMapClass } from './Map/DisplayMapClass';
 import './Hmap.css';
 import GetAddress from '../Home/geocoding';
 import axios from 'axios';
+import { axiosInstance } from '../../AxiosSetUp';
 import { MyRoute } from './RouteMap/Route';
 import Maptest from './Map/maptest';
+const humanizeDuration = require("humanize-duration");
+
 
 
 function Hmap() {
+
+    const shortEnglishHumanizer = humanizeDuration.humanizer({
+        language: "shortEn",
+        languages: {
+          shortEn: {
+            h: () => "hr",
+            m: () => "min",
+          },
+        },
+        serialComma: false,
+        units: ["h", "m"],
+        maxDecimalPoints: 0,
+        conjunction: " "
+    });
 
     const [modalShow, setModalShow] = useState(false)
     const [locQuery, setLocQuery] = useState({})
@@ -21,6 +38,10 @@ function Hmap() {
     const [showMark,setshowMark] = useState(true)
     const [refresh,setrefresh] =useState(true);
     const [toggle,settoggle] = useState(true);
+    const [duration,setduration] = useState();
+    const [distance,setdistance] = useState();
+    const [autoF,setautoF] = useState();
+    const [taxiF,settaxiF] = useState();
 
     const getLocationModal = (type, location, loc) => {
         setLocQuery({
@@ -106,7 +127,52 @@ function Hmap() {
         // document.getElementById("mpBut").style.display = "none";
     }
 
+    const estimateH = async() => {
+        await axios.get('https://router.hereapi.com/v8/routes', {
+            'params': {
+                'apiKey': APP_CODE_HERE,
+                'origin': `${pickupLoc.position.lat},${pickupLoc.position.lng}`,
+                'destination': `${destinationLoc.position.lat},${destinationLoc.position.lng}`,
+                'transportMode': 'car',
+                'return': 'summary,typicalDuration',
+            }
+        }).then((data) => {
+            console.log("data:", data.data.routes[0].sections[0])
+            let dura = data.data.routes[0].sections[0].summary.typicalDuration * 1000;
+            let dis = data.data.routes[0].sections[0].summary.length / 1000;
+            console.log("duration:", shortEnglishHumanizer(Math.ceil(dura)), "len:", dis.toFixed(1));
+            setduration(shortEnglishHumanizer(Math.ceil(dura)))
+            setdistance(dis.toFixed(1));
+        }).catch((e)=> console.log(e));
 
+        //getTaxiFare();
+        document.getElementById("mdRide").style.display="block";
+        
+    }
+
+    const getTaxiFare = async() =>{
+        await axiosInstance.get(`/api/passenger/priceEstimate?time=day&type=Taxi&distance=${distance}`, { withCredentials: true })
+        .then((data) => {
+            console.log("estimation:", data.data.price);
+            settaxiF(data.data.price);
+        }).catch(e=> console.log(e.response));
+    }
+
+    const getAutoFare = async() =>{
+        await axiosInstance.get(`/api/passenger/priceEstimate?time=day&type=Auto&distance=${distance}`, { withCredentials: true })
+        .then((data) => {
+            console.log("estimation:", data.data.price);
+            setautoF(data.data.price);
+        }).catch(e=> console.log(e.response));
+    }
+
+    useEffect(()=>{
+        getTaxiFare()
+    },[distance]);
+
+    useEffect(()=>{
+        getAutoFare()
+    },[distance]);
     
 
 return (
@@ -143,29 +209,44 @@ return (
                     </div>
                     <div className="col-9">
                         {destinationLoc.title ? <span>{destinationLoc.title}</span> : "Select drop location for your ride"}</div>
-                </div>
+                </div><br /><br />
+                <div id='mdRide'>
+                <div className='row mdrides shadow'>
+                    <div className='col-lg-4 mdauto'>
 
-                <button onClick={mpHandler} id="mpBut">Proceed</button>
+                    </div>
+                    <div className='col-lg-4'>
+                        <b>Auto</b><br></br>
+                        Estimated Time: <b>{duration}</b>
+                    </div>
+                    <div className='col-lg-4'>
+                        Estimated Fare: <b>{autoF}</b><br />
+                        Estimated Distance: <b>{distance} km</b>
+                    </div>
+
+                </div>  
+                <div className='row mdrides shadow'>
+                    <div className='col-lg-4 mdtaxi'>
+
+                    </div>
+                    <div className='col-lg-4'>
+                        <b>Taxi</b><br />
+                        Estimated Time: <b>{duration}</b>
+                    </div>
+                    <div className='col-lg-4'>
+                        Estimated Fare: <b>{taxiF}</b><br />
+                        Estimated Distance: <b>{distance} km</b>
+                    </div>
+
+                </div> 
+                </div>  
+
+                <button onClick={estimateH}  id="mpBut">Proceed</button>
             </div>
             <GetAddress locquery={locQuery} show={modalShow} onHide={() => setModalShow(false)}/>
             
         </div>
-        <div className='container-fluid srchB'>
-            <div className='container bg-purple srchR rounded shadow'>
-                <div className='row'>
-                    <div className='col-lg-4'>
-
-                    </div>
-                    <div className='col-lg-4'>
-
-                    </div>
-                    <div className='col-lg-4'>
-
-                    </div>
-
-                </div>   
-            </div>
-        </div>
+        
         
     </div>
 );
